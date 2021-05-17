2b1904cfffa8922ba2a8d23e4887eee1bff83820
franmsn2011 <51340801+franmsn2011@users.noreply.github.com>
2021-05-17 19:02:14 -0300

    creo directorio

---
+++ src/main/java/objetos2/demo/ProyectCuatrimestralApplication.java
@@ -0,0 +1,13 @@
+package objetos2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectCuatrimestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectCuatrimestralApplication.class, args);
+	}
+
+}
