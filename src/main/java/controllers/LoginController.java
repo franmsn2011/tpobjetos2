7285b1157749e17c9b4f947e79a318acfb257111
franmsn2011 <51340801+franmsn2011@users.noreply.github.com>
2021-05-19 19:38:32 -0300

    intento agregar primera vista de login que saque de boostrap

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	
+	//@GetMapping("/index")
+	@GetMapping("/arranca")
+	public String devolverformulario() {
+		return "indexx";
+	}
+	@GetMapping("/")
+	public String holam() {
+		return "hola";
+	}
+	/*
+	@GetMapping("/res")
+	public String traerPersonas(Model model) {
+		model.addAttribute("p",traerPersonas());
+		return "resultado2";
+	}
+		
+	@PostMapping("/resultado")
+	public String resultado(Persona persona, Model model) {
+		model.addAttribute("persona", persona);
+		model.addAttribute("resultado", "resultado del formulario");
+		return "resultado";
+	}
+	public java.util.List<Persona> traerPersonas(){
+		java.util.List<Persona> p= new ArrayList<>();
+		p.add(new Persona("juan", 1234, "gomez", 421111, false));
+		p.add(new Persona("maria", 2345, "apellido", 422222, false));
+		p.add(new Persona("franco", 2345, "gonzales", 3244544, true));
+		p.add(new Persona("pedro", 44335343, "falso", 123432, true));
+		return p;
+	}
+	*/
+}
